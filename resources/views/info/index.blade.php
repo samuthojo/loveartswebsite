073dc5fa846a4a043ec21e61c8d444cd5b5c034e
Walter Kimaro <wakyj07@gmail.com>
2017-07-30 16:25:01 +0300

    Street data mobile page

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -1,165 +1,32 @@
 @extends('layouts.app-inner')
 
+@section('styles')
+    <link rel="stylesheet" href="{{asset('css/infos.css')}}">
+@endsection
+
 @section('content')
-    <style>
-        body{
-            background: #ff4d4d;
-            background: #f0f0f0;
-        }
-
-        #bannerTopBar{
-            position: absolute !important;
-            background-color: transparent;
-            /*background-color: rgba(0,0,0,1);*/
-            padding-top: 20px;
-            padding-bottom: 20px;
-        }
-
-        #bannerTopBar a{
-            color: #000 !important;
-        }
-
-        #bannerTopBar a:hover{
-            background-color: rgba(255, 255, 255, 0.45) !important;
-        }
-
-        #bannerTopBar a.active{
-            background-color: rgba(255, 255, 255, 0.9) !important;
-        }
-
-        #authOptions a{
-            background-color: rgba(5, 5, 5, 0.05) !important;
-            color: #000 !important;
-        }
-
-        .container{
-            max-width: 1000px;
-            margin: auto;
-        }
-
-        #infos{
-            position: relative;
-            padding: 3em 0;
-            padding-bottom: 5em;
-            min-height: 100vh;
-        }
-
-        #infos #title{
-            padding: 3em 0;
-            padding-bottom: 5em;
-        }
-
-        #infos #title h1{
-            font-size: 3em;
-            font-family: "Gotham ultra", sans-serif;
-            /*letter-spacing: 3px;*/
-        }
-
-        #theInfos{
-            padding: 10em 0;
-        }
-
-        .info-image{
-            width: 400px;
-            margin-bottom: 6em;
-        }
-
-        .info-image:nth-child(even){
-            transform: translateY(250px);
-        }
-
-        .info-image .image{
-            position: relative;
-            margin: auto;
-            width: 80%;
-            background-color: #fff;
-            /*height: 480px;*/
-            overflow: hidden;
-            box-shadow: 0 0 30px rgba(0,0,0,0.2);
-            border-radius: 3px;
-            transition: 0.35s transform ease-out;
-        }
-
-        .info-image .image:hover{
-            transform: scale(1.2);
-        }
-
-        .info-image .image img:not(.gify){
-            min-width:200%;
-            min-height: 200%;
-            max-width:200%;
-            max-height: 200%;
-        }
-
-        .info-image .image:hover img{
-            transition: 0.25s transform ease-out;
-        }
-        .info-image .image:hover img:not(.gify){
-            transform: scale(0.5);
-        }
-
-        .info-image .title{
-            font-size: 1.2em;
-            margin-top: 30px;
-            text-align: center;
-            color: #000;
-        }
-
-        .info-image .image-subtitle{
-            display: block;
-            font-size: 0.8em;
-            margin-top: 5px;
-            text-align: center;
-            width: 100%;
-            overflow: hidden;
-            -ms-text-overflow: ellipsis;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-        }
-
-        .info-image .title,
-        .info-image .image-subtitle{
-            transition: 0.25s opacity ease-out;
-        }
-
-        .info-image .image:hover + .title,
-        .info-image .image:hover ~ .image-subtitle{
-            opacity: 0;
-        }
-
-        .info-image .image .scrim{
-            position: absolute;
-            left: 0;
-            top: 0;
-            height: 100%; width: 100%;
-        }
-
-        .info-image .image:not(:hover) .gify{
-            /*opacity: 0;*/
-        }
-    </style>
-
     <div id="infos">
         <div class="container" style="padding-top: 2em;">
             <div class="layout">
-                <div style="width: 75vw; height: 34vw; background-colo: #e5e5e5; border: 1px solid #aaa; margin-right: -48vw; margin-lef: 4vw;"></div>
+                <div id="leftyImage" style="width: 75vw; height: 34vw; background-colo: #e5e5e5; border: 1px solid #aaa; margin-right: -48vw; margin-lef: 4vw;"></div>
                 <div id="title" class="flex" style="padding-top: 8em; background: #f0f0f0;">
                     <h1>
                         Be informed
                     </h1>
-                    <p style="margin-top: 10px; font-size: 1.7em; font-family: 'Gotham light', sans-serif; line-height: 1.5em">
+                    <p>
                         (or <span style="color: var(--app-main-color)">Sanuka</span> as they say),
                         there are beautiful art joints and experiences happening all around you, keep an open eye and give an 'ooooh yeah' nod every now and then.
                     </p>
                 </div>
             </div>
 
-            <div style="text-align: right; display: inline-block; position: absolute; right: 120px;">
-                <hr style="height: 200px; width: 0; margin: auto; margin-bottom: 60px; margin-top: -30px;">
-                <span style="font-size: 1.3em; font-family: 'Gotham light', sans-serif; line-height: 1.5em; display: inline-block; max-width: 300px;">
+            <div id="pointerDeco">
+                <hr>
+                <span>
                     Here are some few that will get you properly hooked up
                 </span>
             </div>
+
             <div id="theInfos" class="layout justified wrap">
                 @foreach($infos as $info)
                     <div class="info-image">
--- resources/views/layouts/app-inner.blade.php
+++ resources/views/layouts/app-inner.blade.php
@@ -18,6 +18,115 @@
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/flex.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        #bannerTopBar{
+            z-index: 99;
+            width: 100%;
+            background: #ff4d4d;
+            padding: 10px 42px;
+            position: fixed;
+            top: 0;
+        }
+
+        #bannerTopBar #title {
+            font-size: 32px;
+            display: inline-block;
+        }
+
+        #bannerTopBar #title img{
+            height: 45px !important;
+        }
+
+        #bannerTopBar #links, #bannerTopBar #authOptions{
+            margin-top: 10px;
+        }
+
+        #bannerTopBar #links > a, #bannerTopBar #authOptions a{
+            padding: 12px 20px;
+            font-family: "Gotham medium", sans-serif;
+            font-weight: 200;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+            font-size: 0.75em;
+            margin-bottom: 11px;
+            color: #fff;
+        }
+
+        #bannerTopBar #authOptions > a{
+            background-color: rgba(255,255,255,0.1);
+            color: #fff;
+        }
+
+        #bannerTopBar #authOptions > a:hover{
+            background-color: rgba(255,255,255,0.2);
+            color: #fff;
+        }
+
+        #bannerTopBar #links > a:hover{
+            background-color: rgba(255,255,255,0.2);
+        }
+
+        #bannerTopBar #links > a.active{
+            background-color: rgb(232, 74, 74);
+        }
+
+        #authOptions img{
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            background-color: #ddd;
+            box-shadow: 0 0 20px rgba(0,0,0,0.1);
+            border: 2px solid #fff;
+            margin-top: -10px;
+        }
+
+        #authOptions #miniProfile{
+            padding: 20px;
+            padding-right: 70px;
+            padding-top: 25px;
+            padding-bottom: 30px;
+            background-color: #fff;
+            box-shadow: 0 0 50px rgba(0,0,0,0.2);
+            position: absolute;
+            right: -16px;
+            top: -26px;
+            color: #000000;
+            opacity: 0;
+            pointer-events: none;
+            transition: all 0.25s ease-out;
+        }
+
+        #authOptions:hover #miniProfile{
+            opacity: 1;
+            pointer-events: auto;
+        }
+
+        #loggedUserName{
+            display: block;
+            letter-spacing: 0 !important;
+            color: #000 !important;
+            text-transform: none !important;
+            font-family: "Gotham medium", sans-serif !important;
+            font-size: 1em !important;
+            margin-bottom: 15px !important;
+            white-space: nowrap;
+            padding-left: 0 !important;
+            padding-top: 0 !important;
+        }
+
+        #authOptions img{
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            background-color: #ddd;
+            box-shadow: 0 0 20px rgba(0,0,0,0.1);
+            border: 2px solid #fff;
+            position: relative;
+            z-index: 1
+        }
+    </style>
     @yield('styles')
 
     <!-- Scripts -->
