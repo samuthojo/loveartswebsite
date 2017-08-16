78ee76521957a29366f6986ec2390729d1e51abd
Walter Kimaro <wakyj07@gmail.com>
2017-08-16 08:28:58 +0300

    Add mawazo challenge link

--- resources/views/layouts/nav_links.blade.php
+++ resources/views/layouts/nav_links.blade.php
@@ -2,7 +2,7 @@
 <a href="{{url('/about')}}">ABOUT</a>
 <a href="{{url('/artists')}}">ARTISTS</a>
 <a href="{{url('/media')}}">MEDIA</a>
-<a href="#artchallenge">ARTS CHALLENGE</a>
+<a href="http://challenges.loveartstanzania.com/">ARTS CHALLENGE</a>
 <a href="{{url('/info')}}">STREET DATA</a>
 <a href="{{url('/events')}}">EVENTS</a>
 {{--<a href="#contact">CONTACTS</a>--}}
